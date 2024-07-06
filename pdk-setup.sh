95730af500a8984f19181c9c9a26b1b23739b937
Noritsuna Imamura <noritsuna@siprop.org>
2024-07-06 21:00:36 +0900

    Chang tab -> 2space.
    Chang tab -> 2space.

--- pdk-setup.sh
+++ pdk-setup.sh
@@ -78,11 +78,11 @@ cd $SRC_DIR
 # Copy KLayout Configurations
 # ----------------------------------
 if [ ! -d "$HOME/.klayout" ]; then
-	mkdir -p $HOME/.klayout/salt
-	mkdir $HOME/.klayout/macros
-	mkdir $HOME/.klayout/drc
-	mkdir $HOME/.klayout/pymacros
-	mkdir $HOME/.klayout/libraries
+  mkdir -p $HOME/.klayout/salt
+  mkdir $HOME/.klayout/macros
+  mkdir $HOME/.klayout/drc
+  mkdir $HOME/.klayout/pymacros
+  mkdir $HOME/.klayout/libraries
 fi
 
 
@@ -109,7 +109,7 @@ python3 -m pip install --upgrade --no-cache-dir volare
 # Install/update xschem
 # ---------------------
 if [ ! -d "$SRC_DIR/xschem" ]; then
-	echo ">>>> Installing xschem"
+  echo ">>>> Installing xschem"
   if [ "$(uname)" == 'Darwin' ]; then
     OS='Mac'
     brew install libx11
@@ -117,8 +117,8 @@ if [ ! -d "$SRC_DIR/xschem" ]; then
     brew install gawk
     brew install macvim
     brew install dbus
-	  brew install --cask xquartz
-	  brew install libglu freeglut
+    brew install --cask xquartz
+    brew install libglu freeglut
 
     cd $SRC_DIR
     wget https://prdownloads.sourceforge.net/tcl/tcl$TCL_VERSION-src.tar.gz
@@ -141,11 +141,11 @@ if [ ! -d "$SRC_DIR/xschem" ]; then
     make clean
   elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     OS='Linux'
-	  sudo apt -qq install -y xterm graphicsmagick ghostscript \
-	  libx11-6 libx11-dev libxrender1 libxrender-dev \
-	  libxcb1 libx11-xcb-dev libcairo2 libcairo2-dev  \
-	  tcl8.6 tcl8.6-dev tk8.6 tk8.6-dev \
-	  flex bison libxpm4 libxpm-dev gawk tcl-tclreadline
+    sudo apt -qq install -y xterm graphicsmagick ghostscript \
+    libx11-6 libx11-dev libxrender1 libxrender-dev \
+    libxcb1 libx11-xcb-dev libcairo2 libcairo2-dev  \
+    tcl8.6 tcl8.6-dev tk8.6 tk8.6-dev \
+    flex bison libxpm4 libxpm-dev gawk tcl-tclreadline
   elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then                                                                                           
     OS='Cygwin'
     echo "Your platform ($(uname -a)) is not supported."
@@ -154,13 +154,13 @@ if [ ! -d "$SRC_DIR/xschem" ]; then
     echo "Your platform ($(uname -a)) is not supported."
     exit 1
   fi
-	git clone https://github.com/StefanSchippers/xschem.git "$SRC_DIR/xschem"
-	cd "$SRC_DIR/xschem" || exit
-	./configure
+  git clone https://github.com/StefanSchippers/xschem.git "$SRC_DIR/xschem"
+  cd "$SRC_DIR/xschem" || exit
+  ./configure
 else
-	echo ">>>> Updating xschem"
-	cd "$SRC_DIR/xschem" || exit
-	git pull
+  echo ">>>> Updating xschem"
+  cd "$SRC_DIR/xschem" || exit
+  git pull
 fi
 make clean
 make -j"$(nproc)" && sudo make install
@@ -170,11 +170,11 @@ make clean
 # Install/update xschem-gaw
 # -------------------------
 if [ ! -d "$SRC_DIR/xschem-gaw" ]; then
-	echo ">>>> Installing gaw"
+  echo ">>>> Installing gaw"
   if [ "$(uname)" == 'Darwin' ]; then
     OS='Mac'
     brew install pkg-config
- 	  brew install automake
+     brew install automake
     brew install autoconf
     brew install gtk+3
     gettextize -f
@@ -202,21 +202,21 @@ if [ ! -d "$SRC_DIR/xschem-gaw" ]; then
     echo "Your platform ($(uname -a)) is not supported."
     exit 1
   fi
-	git clone https://github.com/StefanSchippers/xschem-gaw.git "$SRC_DIR/xschem-gaw"
-	cd "$SRC_DIR/xschem-gaw" || exit
- 	aclocal && automake --add-missing && autoconf
+  git clone https://github.com/StefanSchippers/xschem-gaw.git "$SRC_DIR/xschem-gaw"
+  cd "$SRC_DIR/xschem-gaw" || exit
+   aclocal && automake --add-missing && autoconf
   if [ "$(uname)" == 'Darwin' ]; then
     OS='Mac'
 #    export LDFLAGS="-L/usr/X11/lib $LDFLAGS"
     ./configure --enable-gawsound=no LDFLAGS="-L/usr/X11/lib"
   elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     OS='Linux'
-	  #  FIXME this is just a WA for 22.04 LTS
-	  sed -i 's/GETTEXT_MACRO_VERSION = 0.18/GETTEXT_MACRO_VERSION = 0.20/g' po/Makefile.in.in
+    #  FIXME this is just a WA for 22.04 LTS
+    sed -i 's/GETTEXT_MACRO_VERSION = 0.18/GETTEXT_MACRO_VERSION = 0.20/g' po/Makefile.in.in
     ./configure
   fi
 else
-	echo ">>>> Updating gaw"
+  echo ">>>> Updating gaw"
   cd "$SRC_DIR/xschem-gaw" || exit
   git pull
 fi
@@ -236,22 +236,22 @@ if [ "$(uname)" == 'Darwin' ]; then
 #  cd /Volumes/KLayout/
 #  hdiutil detach /Volumes/KLayout/
   if [ ! -d "$SRC_DIR/klayout" ]; then
-	  echo ">>>> Building klayout"
+    echo ">>>> Building klayout"
     brew install qt5
     brew install libgit2
     git clone https://github.com/KLayout/klayout.git "$SRC_DIR/klayout"
     cd "$SRC_DIR/klayout" || exit
   else
-	  echo ">>>> Updating klayout"
+    echo ">>>> Updating klayout"
     cd "$SRC_DIR/klayout" || exit
     git pull
   fi
   python3 build4mac.py -r HB33 -p HBAuto -q Qt5Brew -m ‘—jobs=8’ -n -u
   mkdir -p $HOME/bin/klayout.app
-  cp -aR 	cd $SRC_DIR/klayout/qt5Brew.bin.macos-Sonoma-release-Rhb33Phbauto/* $HOME/bin/klayout.app/
+  cp -aR   cd $SRC_DIR/klayout/qt5Brew.bin.macos-Sonoma-release-Rhb33Phbauto/* $HOME/bin/klayout.app/
   echo 'export PATH="$HOME/bin/:$PATH"' >> ~/.zshrc
   export PATH="$HOME/bin/:$PATH"
-	cp $my_dir/klayout.sh $HOME/bin/
+  cp $my_dir/klayout.sh $HOME/bin/
   chmod +x $HOME/bin/klayout.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   OS='Linux'
@@ -272,13 +272,13 @@ fi
 # Install/update magic
 # --------------------
 if [ ! -d "$SRC_DIR/magic" ]; then
-	echo ">>>> Installing magic"
+  echo ">>>> Installing magic"
   if [ "$(uname)" == 'Darwin' ]; then
     OS='Mac'
   elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     OS='Linux'
-	  sudo apt -qq install -y m4 tcsh csh libx11-dev tcl-dev tk-dev \
-	  libcairo2-dev mesa-common-dev libglu1-mesa-dev
+    sudo apt -qq install -y m4 tcsh csh libx11-dev tcl-dev tk-dev \
+    libcairo2-dev mesa-common-dev libglu1-mesa-dev
   elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then                                                                                           
     OS='Cygwin'
     echo "Your platform ($(uname -a)) is not supported."
@@ -287,14 +287,14 @@ if [ ! -d "$SRC_DIR/magic" ]; then
     echo "Your platform ($(uname -a)) is not supported."
     exit 1
   fi
-	git clone https://github.com/RTimothyEdwards/magic.git "$SRC_DIR/magic"
-	cd "$SRC_DIR/magic" || exit
-	git checkout magic-8.3
-	./configure
+  git clone https://github.com/RTimothyEdwards/magic.git "$SRC_DIR/magic"
+  cd "$SRC_DIR/magic" || exit
+  git checkout magic-8.3
+  ./configure
 else
-	echo ">>>> Updating magic"
-	cd "$SRC_DIR/magic" || exit
-	git pull
+  echo ">>>> Updating magic"
+  cd "$SRC_DIR/magic" || exit
+  git pull
 fi
 make clean
 make && sudo make install
@@ -304,15 +304,15 @@ make clean
 # Install/update netgen
 # ---------------------
 if [ ! -d "$SRC_DIR/netgen" ]; then
-	echo ">>>> Installing netgen"
-	git clone https://github.com/RTimothyEdwards/netgen.git "$SRC_DIR/netgen"
-	cd "$SRC_DIR/netgen" || exit
-	git checkout netgen-1.5
+  echo ">>>> Installing netgen"
+  git clone https://github.com/RTimothyEdwards/netgen.git "$SRC_DIR/netgen"
+  cd "$SRC_DIR/netgen" || exit
+  git checkout netgen-1.5
   ./configure
 else
-	echo ">>>> Updating netgen"
-	cd "$SRC_DIR/netgen" || exit
-	git pull
+  echo ">>>> Updating netgen"
+  cd "$SRC_DIR/netgen" || exit
+  git pull
 fi
 make clean
 make -j"$(nproc)" && sudo make install
@@ -322,7 +322,7 @@ make clean
 # Install/update ngspice
 # ----------------------
 if [ ! -d "$SRC_DIR/ngspice" ]; then
-	echo ">>>> Installing ngspice"
+  echo ">>>> Installing ngspice"
   if [ "$(uname)" == 'Darwin' ]; then
     OS='Mac'
     brew install libtool
@@ -346,8 +346,8 @@ if [ ! -d "$SRC_DIR/ngspice" ]; then
   elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     OS='Linux'
     sudo apt -qq install -y libxaw7-dev libxmu-dev libxext-dev libxft-dev \
-	  libfontconfig1-dev libxrender-dev libfreetype6-dev libx11-dev libx11-6 \
-	  libtool bison flex libreadline-dev libfftw3-dev 
+    libfontconfig1-dev libxrender-dev libfreetype6-dev libx11-dev libx11-6 \
+    libtool bison flex libreadline-dev libfftw3-dev 
   elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then                                                                                           
     OS='Cygwin'
     echo "Your platform ($(uname -a)) is not supported."
@@ -357,19 +357,19 @@ if [ ! -d "$SRC_DIR/ngspice" ]; then
     exit 1
   fi
 
-	git clone http://git.code.sf.net/p/ngspice/ngspice "$SRC_DIR/ngspice"
-	cd "$SRC_DIR/ngspice" || exit
-	./autogen.sh
+  git clone http://git.code.sf.net/p/ngspice/ngspice "$SRC_DIR/ngspice"
+  cd "$SRC_DIR/ngspice" || exit
+  ./autogen.sh
 
   if [ "$(uname)" == 'Darwin' ]; then
     OS='Mac'
-	  ./configure --disable-debug --with-readline=no --enable-openmp --with-x CXX="g++$CXX_VERSION" CC="gcc$CC_VERSION" CFLAGS="-m64 -O2 -Wno-error=implicit-function-declaration -Wno-error=implicit-int" LDFLAGS="-m64 -s" CPPFLAGS="-I/opt/homebrew/opt/freetype2/include/freetype2/ -I/opt/homebrew/opt/libomp/include/" LDFLAGS="-L/opt/homebrew/opt/freetype2/lib/ -L/opt/homebrew/opt/fontconfig/lib/ -L/opt/homebrew/opt/libomp/lib/ -L/usr/X11/lib/"
-	  sed -i '' 's/TCGETS/TIOCMGET/g' src/frontend/parser/complete.c
-	  sed -i '' 's/TCSETS/TIOCMSET/g' src/frontend/parser/complete.c
+    ./configure --disable-debug --with-readline=no --enable-openmp --with-x CXX="g++$CXX_VERSION" CC="gcc$CC_VERSION" CFLAGS="-m64 -O2 -Wno-error=implicit-function-declaration -Wno-error=implicit-int" LDFLAGS="-m64 -s" CPPFLAGS="-I/opt/homebrew/opt/freetype2/include/freetype2/ -I/opt/homebrew/opt/libomp/include/" LDFLAGS="-L/opt/homebrew/opt/freetype2/lib/ -L/opt/homebrew/opt/fontconfig/lib/ -L/opt/homebrew/opt/libomp/lib/ -L/usr/X11/lib/"
+    sed -i '' 's/TCGETS/TIOCMGET/g' src/frontend/parser/complete.c
+    sed -i '' 's/TCSETS/TIOCMSET/g' src/frontend/parser/complete.c
     sed -i '' 's/LEX = :/LEX = lex/g' src/xspice/cmpp/Makefile
   elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     OS='Linux'
-	  ./configure --disable-debug --with-readline=yes --enable-openmp CFLAGS="-m64 -O2" LDFLAGS="-m64 -s" 
+    ./configure --disable-debug --with-readline=yes --enable-openmp CFLAGS="-m64 -O2" LDFLAGS="-m64 -s" 
   elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then                                                                                           
     OS='Cygwin'
     echo "Your platform ($(uname -a)) is not supported."
@@ -379,7 +379,7 @@ if [ ! -d "$SRC_DIR/ngspice" ]; then
     exit 1
   fi
 else
-	echo ">>>> Updating ngspice"
+  echo ">>>> Updating ngspice"
         cd "$SRC_DIR/ngspice" || exit
         git pull
   export PATH="/opt/homebrew/opt/m4/bin:$PATH"
@@ -391,7 +391,9 @@ make clean
 
 # setup gnome-terminal (WA for Ubuntu 24 WSL2)
 # --------
-if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+if [ "$(uname)" == 'Darwin' ]; then
+  OS='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   OS='Linux'
   sudo apt -qq install -y gnome-terminal
   systemctl --user start gnome-terminal-server
@@ -402,32 +404,32 @@ fi
 # ----------------------------------
 if [ ! -d "$SRC_DIR/OpenRule1um" ]; then
         cd $SRC_DIR
-	git clone  https://github.com/mineda-support/OpenRule1um.git
-	cp -aR OpenRule1um $HOME/.klayout/salt/
+  git clone  https://github.com/mineda-support/OpenRule1um.git
+  cp -aR OpenRule1um $HOME/.klayout/salt/
 else
-	echo ">>>> Updating OpenRule1um"
-	cd $SRC_DIR/OpenRule1um || exit
+  echo ">>>> Updating OpenRule1um"
+  cd $SRC_DIR/OpenRule1um || exit
         git pull
-	cp -aR ./* $HOME/.klayout/salt/OpenRule1um/
+  cp -aR ./* $HOME/.klayout/salt/OpenRule1um/
 fi
 if [ ! -d "$SRC_DIR/AnagixLoader" ]; then
         cd $SRC_DIR
-	git clone  https://github.com/mineda-support/AnagixLoader.git
-	cp -aR AnagixLoader $HOME/.klayout/salt/
+  git clone  https://github.com/mineda-support/AnagixLoader.git
+  cp -aR AnagixLoader $HOME/.klayout/salt/
 else
-	echo ">>>> Updating AnagixLoader"
-	cd $SRC_DIR/AnagixLoader || exit
+  echo ">>>> Updating AnagixLoader"
+  cd $SRC_DIR/AnagixLoader || exit
         git pull
-	cp -aR ./* $HOME/.klayout/salt/AnagixLoader/
+  cp -aR ./* $HOME/.klayout/salt/AnagixLoader/
 fi
 
 if [ ! -d "$HOME/.xschem/symbols" ]; then
-	mkdir -p $HOME/.xschem/symbols
-	cd $my_dir
-	cp xschem/xschemrc_PTS06 $HOME/.xschem/xschemrc
-	cp xschem/title_PTS06.sch $HOME/.xschem/title_PTS06.sch
-	cp -aR ./xschem/symbols/* $HOME/.xschem/symbols/
-	cp -aR ./xschem/lib/* $HOME/.xschem/lib/
+  mkdir -p $HOME/.xschem/symbols
+  cd $my_dir
+  cp xschem/xschemrc_PTS06 $HOME/.xschem/xschemrc
+  cp xschem/title_PTS06.sch $HOME/.xschem/title_PTS06.sch
+  cp -aR ./xschem/symbols/* $HOME/.xschem/symbols/
+  cp -aR ./xschem/lib/* $HOME/.xschem/lib/
 fi
 
 # Finished
