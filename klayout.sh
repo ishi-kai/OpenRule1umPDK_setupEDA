95ea31823cb430f1c9340d87841d6d282c4734e4
Noritsuna Imamura <noritsuna@siprop.org>
2025-01-03 16:18:16 +0900

    Update klayout.sh

--- klayout.sh
+++ klayout.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+export DYLD_LIBRARY_PATH="$(brew --prefix ruby@3.3)/lib:$DYLD_LIBRARY_PATH"
+export PATH="$(brew --prefix ruby@3.3)/bin:$PATH"
 cd $HOME/bin/klayout.app/
 ./klayout.app/Contents/MacOS/klayout -e
